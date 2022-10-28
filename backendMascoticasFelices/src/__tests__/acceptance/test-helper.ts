969d6d5fb3f30d1e084d977e6499f8afb7dca101
Laura_Skaphe <lauragonsan7@gmail.com>
2022-10-27 22:17:45 -0500

    segundo commit

---
+++ backendMascoticasFelices/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {Mascoticasfelicesv2Application} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new Mascoticasfelicesv2Application({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: Mascoticasfelicesv2Application;
+  client: Client;
+}
