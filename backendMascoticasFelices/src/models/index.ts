65d9e8f3bb9b8d34069a1fecf66e6b48862b21c5
Laura_Skaphe <lauragonsan7@gmail.com>
2022-10-28 10:46:15 -0500

    Creación modelos

--- backendMascoticasFelices/src/models/index.ts
+++ backendMascoticasFelices/src/models/index.ts
@@ -1,1 +1,6 @@
 export * from './usuario.model';
+export * from './mascota.model';
+export * from './plan.model';
+export * from './sucursal.model';
+export * from './producto-servicio.model';
+export * from './prospecto.model';
---
+++ backendMascoticasFelices/src/models/mascota.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  foto: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estado: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  especie: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  comentario: string;
+
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
